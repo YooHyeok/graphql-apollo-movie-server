8c8da33736e52844a7911ed15b0627f1f980ec68
webdevyoo <yjk7454@naver.com>
2024-02-04 00:03:48 +0900

    feat: sertver.js 사용자 지정 type 구현 및 root type에 적용

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import {ApolloServer, gql} from "apollo-server"
 
 const typeDefs = gql`
-  type Query {
-    text: String
-    hello: String
+  type User { # 사용자 지정 타입으로 데이터베이스의 관계에 따라 결정된다.
+    id: ID
+    username: String # 이러한 타입을 ScalarType이라고 한다.
   }
+
+  type Tweet { # allTweets이 반환하는 사용자 지정 type이다.
+    id: ID
+    text: String
+    author: User
+  }
+
+  type Query { # root type
+    allTwwets: [Tweet] # Tweet 타입 형태의 List를 반환한다.
+    tweet: Tweet
+  }
+  
 ` //SDL :Schema Definition Language
 
 const server = new ApolloServer({typeDefs})
