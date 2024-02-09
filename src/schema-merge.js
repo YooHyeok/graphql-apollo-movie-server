3f10d2954875223f5329053e9badad2fb7e9fc85
YooHyeok <yjk7454@naver.com>
2024-02-10 01:18:23 +0900

    refactor: 스키마 병합 코드 축소

--- src/schema-merge.js
+++ src/schema-merge.js
@@ -1,50 +1,22 @@
-import { loadFilesSync, loadFiles  } from "@graphql-tools/load-files";
+import { loadFiles } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-
 import path from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
 
-
-// const typeDefsArray = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-// const resolversArray = loadFilesSync(`${__dirname}/**/*.resolver.js`);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const typeDefsPath = path.join(
-    __dirname, '**/*.typeDefs.js'
-);
-const resolversPath = path.join(
-    __dirname, '**/*.resolver.js'
-);
-
-
-const typeDefsArray = await loadFiles(typeDefsPath, {
-    useRequire: true,
-    requireMethod: async (path) => {
-        console.debug('Using typeDefs at:', path);
-        return await import(pathToFileURL(path));
-    }
-});
-const resolversArray = await loadFiles(resolversPath, {
-    useRequire: true,
-    requireMethod: async (path) => {
-        console.debug('Using resolver at:', path);
-        return await import(pathToFileURL(path));
-    }
-});
-
-
 // typeDefs과 reolvers의 파일 정의(각 파일들의 추적에 사용)
+const typeDefsArray = await loadFiles(`${path.dirname(fileURLToPath(import.meta.url))}/**/*.typeDefs.js`, {
+    useRequire: true,
+    requireMethod: async path => await import(pathToFileURL(path))
+});
+const resolversArray = await loadFiles(`${path.dirname(fileURLToPath(import.meta.url))}/**/*.resolver.js`, {
+    useRequire: true,
+    requireMethod: async path => await import(pathToFileURL(path))
+});
 
-const mergedTypeDefs = mergeTypeDefs(typeDefsArray);
-const mergedResolvers = mergeResolvers(resolversArray);
-// 추적된 파일들을 통합
-console.log(mergedTypeDefs)
 const schema = makeExecutableSchema({
-  typeDefs: mergedTypeDefs,
-  resolvers: mergedResolvers,
+  typeDefs: mergeTypeDefs(typeDefsArray),// 추적된 파일들을 통합
+  resolvers: mergeResolvers(resolversArray),// 추적된 파일들을 통합
 });
 // 추적된 파일들을 사용가능하도록 분류
 
